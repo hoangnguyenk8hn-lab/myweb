fd145b6634dd5b0544e0774b39d838f7fff7efb4
hoangnguyenk8hn-lab <hoangnguyenk8hn@gmail.com>
2026-09-10 23:27:17 +0700

    Keep endpoint/intersection snapping with Option/Alt

--- app/diagram/intersections.ts
+++ app/diagram/intersections.ts
@@ -1,17 +1,1 @@
-import type { DiagramElement, Point } from "./types";
-import { dropLineEndpoint as baseDropLineEndpoint } from "./intersectionsBase";
-import { isAltModifierDown } from "./modifierState";
-
 export * from "./intersectionsBase";
-
-export function dropLineEndpoint(
-  e: DiagramElement,
-  index: number,
-  point: Point,
-  mark: { point: Point } | null,
-  nodeId?: string,
-): DiagramElement {
-  return isAltModifierDown()
-    ? baseDropLineEndpoint(e, index, point, null, undefined)
-    : baseDropLineEndpoint(e, index, point, mark, nodeId);
-}
